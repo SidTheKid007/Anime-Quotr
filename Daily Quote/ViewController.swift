33ea76f6d0bf922def355f46e9d4699d97142438
Siddhu K <siddhesvark@gmail.com>
2020-08-02 16:51:47 -0500

    Initial Commit

---
+++ Daily Quote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Daily Quote
+//
+//  Created by Siddhesvar Kannan on 8/2/20.
+//  Copyright © 2020 Siddhesvar Kannan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
